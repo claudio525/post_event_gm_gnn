fddd397bb34c93d359ea6854c0787efaa8dcc2df
Claudio <claudio.schill@canterbury.ac.nz>
2023-11-07 14:33:24 +1300

    Added k-fold support + refactor

--- sim_ranking/ml/pairwise.py
+++ sim_ranking/ml/pairwise.py
@@ -11,19 +11,12 @@ import torch.functional as F
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import pandas as pd
-import typer
 from torchinfo import summary
 from tqdm import tqdm, trange
 
 import ml_tools as mlt
 import sim_ranking as sr
 import spatial_hazard as sh
-
-device = "cpu"
-if torch.cuda.is_available():
-    device = "cuda"
-
-print(f"Using device: {device}")
 
 
 def compute_res_area(rs_obs: np.ndarray, rs_sim: np.ndarray):
@@ -190,7 +183,7 @@ class PairDataset(Dataset):
             ) / self.pSA_std[self.pSA_keys].values[None, :, None]
 
     def __len__(self):
-        return np.sum(self.n_samples_event)
+        return int(np.sum(self.n_samples_event))
 
     def __getitem__(self, idx: int):
         # Have to it this way, as some events may not have samples
@@ -245,15 +238,12 @@ class PairDataset(Dataset):
 @dataclass
 class RunParamsConfig:
     max_dist: float
-    epoch_size: int
     max_n_rels: int
 
     debug: bool
     device: str
 
     results_dir = Path(os.path.expandvars("$wdata/sim_ranking/results/ml"))
-
-
 
 
 def train(
@@ -386,14 +376,14 @@ def train(
         print(f"\tValidation - Loss: {metrics['loss_hist_val'][epoch_ix]:.4f}, Accuracy: {metrics['acc_hist_val'][epoch_ix]:.4f}")
 
 def get_prediction(
-    ranking_model: nn.Module,
-    scalar_features: torch.Tensor,
-    int_sim_pSA_rel_1: torch.Tensor,
-    int_sim_pSA_rel_2: torch.Tensor,
-    obs_sim_pSA_rel_1: torch.Tensor,
-    obs_sim_pSA_rel_2: torch.Tensor,
-    obs_obs_pSA: torch.Tensor,
-    device: str,
+        ranking_model: nn.Module,
+        scalar_features: torch.Tensor,
+        int_sim_pSA_rel_1: torch.Tensor,
+        int_sim_pSA_rel_2: torch.Tensor,
+        obs_sim_pSA_rel_1: torch.Tensor,
+        obs_sim_pSA_rel_2: torch.Tensor,
+        obs_obs_pSA: torch.Tensor,
+        device: str,
 ):
     pSA_data = torch.cat(
         (
@@ -413,57 +403,35 @@ def get_prediction(
     return pred
 
 
-def main(hyperparams_ffp: Path, max_dist: float = 75, debug: bool = False, epoch_size: int = 1_000_000, max_n_rels: int = 25):
+def data_prep(
+    event_sites: Dict[str, np.ndarray],
+    train_events: np.ndarray,
+    val_events: np.ndarray,
+    train_sites: np.ndarray,
+    val_sites: np.ndarray,
+    events: np.ndarray,
+    run_config: RunParamsConfig,
+    db: sr.db.DB,
+):
     ### Constants
     # Scalar features
-    # SITE_FEATURE_KEYS = ["vs30", "z1.0", "z2.5", "tsite"]
-    SITE_FEATURE_KEYS = ["vs30"]
+    SITE_FEATURE_KEYS = ["vs30", "z1.0", "z2.5", "tsite"]
+    # SITE_FEATURE_KEYS = ["vs30"]
     SITE_TO_SITE_FEATURE_KEYS = ["dist"]
-    # EVENT_SITE_FEATURE_KEYS = ["r_rup"]
-    EVENT_SITE_FEATURE_KEYS = []
+    EVENT_SITE_FEATURE_KEYS = ["r_rup"]
+    # EVENT_SITE_FEATURE_KEYS = []
     EVENT_SITE_TO_SITE_FEATURE_KEYS = ["angular_dist"]
 
     WEIGHT_SITE_TO_SITE_FEATURE_KEYS = ["dist", "vs30_dist"]
     WEIGHT_EVENT_SITE_TO_SITE_FEATURE_KEYS = ["angular_dist"]
 
-    run_config = RunParamsConfig(max_dist, epoch_size, max_n_rels, debug, device)
-    hp_config = HyperParamsConfig.from_yaml(hyperparams_ffp)
-
-    ### Data loading
-    # db_ffp_orig = "$wdata/sim_ranking/db/gm_db.sqlite"
-    db_ffp_orig = "$wdata/sim_ranking/db/gm_db_neil.sqlite"
-    db_ffp = Path(os.path.expandvars(db_ffp_orig))
-
-    db = sr.db.DB(db_ffp)
-    station_df = db.get_site_df()
     event_df = db.get_event_df()
     record_df = db.get_record_df()
 
-    # events = db.get_avail_events(data_source="specific")
-    events = db.get_avail_events(data_source="neil")
-    print(f"Number of events: {len(events)}")
-
-    # Get all relevant sites across all events
-    # all_sites = np.unique(np.concatenate(list(event_sites.values())))
-    all_sites = db.get_avail_sites()
-
-    ### Data setup
-    # Compute the distance matrix
     print(f"Computing distance matrix")
+    station_df = db.get_site_df()
+    all_sites = db.get_avail_sites()
     dist_matrix = sh.im_dist.calculate_distance_matrix(all_sites, station_df)
-
-    # Get the sites per event
-    event_sites = db.get_event_sites()
-
-    # TODO: Fix this!!
-    train_sites = all_sites
-    val_sites = all_sites
-
-    # val_events = np.asarray(["3468575"])
-    # val_events = np.asarray(["3468575", "2016p118944", "3525264", "3528839"])
-    # np.random.seed(42)
-    val_events = np.random.choice(events, 10, replace=False)
-    train_events = np.setdiff1d(events, val_events)
 
     # Get the training and validation dataset site combinations
     print(f"Creating site combinations")
@@ -547,7 +515,7 @@ def main(hyperparams_ffp: Path, max_dist: float = 75, debug: bool = False, epoch
         scalar_features,
         pSA_mean,
         pSA_std,
-        run_config.max_n_rels
+        run_config.max_n_rels,
     )
 
     val_dataset = PairDataset(
@@ -559,31 +527,18 @@ def main(hyperparams_ffp: Path, max_dist: float = 75, debug: bool = False, epoch
         scalar_features,
         pSA_mean,
         pSA_std,
-        run_config.max_n_rels
+        run_config.max_n_rels,
     )
 
     print(f"Number of training samples: {len(train_dataset)}")
     print(f"Number of validation samples: {len(val_dataset)}")
 
-    # # Create the dataloaders
-    # N_DATALOADER_WORKERS = 0 if run_config.debug else 8
-    # train_dataloader = DataLoader(
-    #     train_dataset,
-    #     batch_size=hp_config.batch_size,
-    #     shuffle=True,
-    #     num_workers=N_DATALOADER_WORKERS,
-    #     pin_memory=True,
-    #     persistent_workers=True if N_DATALOADER_WORKERS > 0 else False,
-    # )
-    # val_dataloader = DataLoader(
-    #     val_dataset,
-    #     batch_size=hp_config.batch_size * 50,
-    #     shuffle=True,
-    #     num_workers=N_DATALOADER_WORKERS,
-    #     pin_memory=True,
-    #     persistent_workers=True if N_DATALOADER_WORKERS > 0 else False,
-    # )
+    return train_dataset, val_dataset, scalar_features
 
+
+def create_model(
+    hp_config: HyperParamsConfig, scalar_features: sr.ml.data.ScalarFeatures
+):
     # Create the model
     n_conv_layers = len(hp_config.kernel_sizes)
     padding = (
@@ -615,11 +570,4 @@ def main(hyperparams_ffp: Path, max_dist: float = 75, debug: bool = False, epoch
         ],
     )
 
-    # Train
-    train(ranking_model, train_dataset, val_dataset, device, hp_config, run_config)
-
-    print(f"wtf")
-
-
-if __name__ == "__main__":
-    typer.run(main)
+    return ranking_model
---
+++ sim_ranking/ml_models/run_pairwise_ranking.py
@@ -0,0 +1,235 @@
+import os
+from pathlib import Path
+
+import torch
+import numpy as np
+import pandas as pd
+import typer
+from scipy.cluster import hierarchy
+from scipy.spatial import distance
+from plotly import graph_objects as go
+
+import sim_ranking as sr
+import spatial_hazard as sh
+
+from sim_ranking.ml import pairwise as pr
+
+app = typer.Typer()
+
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+
+print(f"Using device: {device}")
+
+
+@app.command("train-model")
+def train_model(
+    hyperparams_ffp: Path,
+    max_dist: float = 75,
+    debug: bool = False,
+    max_n_rels: int = 25,
+):
+    """Trains a single model"""
+    run_config = pr.RunParamsConfig(max_dist, max_n_rels, debug, device)
+    hp_config = pr.HyperParamsConfig.from_yaml(hyperparams_ffp)
+
+    ### Data loading
+    # db_ffp_orig = "$wdata/sim_ranking/db/gm_db.sqlite"
+    db_ffp_orig = "$wdata/sim_ranking/db/gm_db_neil.sqlite"
+    db_ffp = Path(os.path.expandvars(db_ffp_orig))
+
+    db = sr.db.DB(db_ffp)
+
+    # events = db.get_avail_events(data_source="specific")
+    events = db.get_avail_events(data_source="neil")
+    print(f"Number of events: {len(events)}")
+
+    # Get all relevant sites across all events
+    # all_sites = np.unique(np.concatenate(list(event_sites.values())))
+    all_sites = db.get_avail_sites()
+
+    ### Data setup
+
+    # Get the sites per event
+    event_sites = db.get_event_sites()
+
+    # TODO: Fix this!!
+    train_sites = all_sites
+    val_sites = all_sites
+
+    # val_events = np.asarray(["3468575"])
+    # val_events = np.asarray(["3468575", "2016p118944", "3525264", "3528839"])
+    np.random.seed(30)
+    val_events = np.random.choice(events, 10, replace=False)
+    train_events = np.setdiff1d(events, val_events)
+
+    # Data prep
+    train_dataset, val_dataset, scalar_features = pr.data_prep(
+        event_sites,
+        train_events,
+        val_events,
+        train_sites,
+        val_sites,
+        events,
+        run_config,
+        db,
+    )
+
+    # Create the model
+    ranking_model = pr.create_model(hp_config, scalar_features)
+
+    # Train
+    pr.train(ranking_model, train_dataset, val_dataset, device, hp_config, run_config)
+
+
+@app.command("run-kfold")
+def run_kfold(
+    hyperparams_ffp: Path,
+    site_folds_ffp: Path,
+    max_dist: float = 75,
+    debug: bool = False,
+    max_n_rels: int = 25,
+):
+    """
+    Uses k-fold to get a better estimate of the model performance
+    """
+    run_config = pr.RunParamsConfig(max_dist, max_n_rels, debug, device)
+    hp_config = pr.HyperParamsConfig.from_yaml(hyperparams_ffp)
+
+    ### Data loading
+    # db_ffp_orig = "$wdata/sim_ranking/db/gm_db.sqlite"
+    db_ffp_orig = "$wdata/sim_ranking/db/gm_db_neil.sqlite"
+    db_ffp = Path(os.path.expandvars(db_ffp_orig))
+
+    db = sr.db.DB(db_ffp)
+
+    # Get all relevant sites across all events
+    all_sites = db.get_avail_sites()
+
+    # Get the fold clusters
+    folds_labels = pd.read_csv(site_folds_ffp, index_col=0).squeeze()
+
+    # Sanity check
+    assert np.all(all_sites == folds_labels.index.values)
+
+    # Some data loading
+    events = db.get_avail_events(data_source="neil")
+    print(f"Number of events: {len(events)}")
+
+    obs_df = db.get_obs_df()
+    obs_df = obs_df.loc[np.isin(obs_df.event_id, events)]
+
+    # Get the sites per event
+    event_sites = db.get_event_sites()
+
+    for cur_fold_id in np.unique(folds_labels.values):
+        # Get the sites for this fold
+        cur_val_sites = folds_labels.index.values[
+            folds_labels.values == cur_fold_id
+        ].astype(str)
+        cur_train_sites = np.setdiff1d(all_sites, cur_val_sites)
+
+        cur_obs_df = obs_df.loc[np.isin(obs_df.site_id, cur_val_sites)]
+        cur_val_events = (
+            cur_obs_df[["event_id", "site_id"]]
+            .groupby("event_id")
+            .count()
+            .sort_values("site_id")
+            .tail(25)
+            .index.values
+        )
+        cur_train_events = np.setdiff1d(events, cur_val_events)
+
+        # Data prep
+        train_dataset, val_dataset, scalar_features = pr.data_prep(
+            event_sites,
+            cur_train_events,
+            cur_val_events,
+            cur_train_sites,
+            cur_val_sites,
+            events,
+            run_config,
+            db,
+        )
+
+        # Create the model
+        ranking_model = pr.create_model(hp_config, scalar_features)
+
+        # Train
+        pr.train(
+            ranking_model, train_dataset, val_dataset, device, hp_config, run_config
+        )
+
+
+@app.command("gen-site-fold-clusters")
+def gen_site_fold_clusters(db_ffp: Path, output_dir: Path, n_clusters: int = 4):
+    def compute_hierarchical_linkage_matrix(dist_mat, method="complete"):
+        if method == "complete":
+            return hierarchy.complete(dist_mat)
+        if method == "single":
+            return hierarchy.single(dist_mat)
+        if method == "average":
+            return hierarchy.average(dist_mat)
+        if method == "ward":
+            return hierarchy.ward(dist_mat)
+
+    # Load data
+    db = sr.db.DB(db_ffp)
+    sites = db.get_avail_sites()
+    station_df = db.get_site_df()
+
+    # Compute distance matrix
+    dist_matrix = sh.im_dist.calculate_distance_matrix(sites, station_df)
+
+    # Run hierarchical clustering
+    condensed_dist_matrix = distance.squareform(dist_matrix.values)
+    Z = compute_hierarchical_linkage_matrix(condensed_dist_matrix, method="ward")
+
+    cluster_labels = hierarchy.fcluster(Z, n_clusters, criterion="maxclust")
+
+    # Save the cluster labels
+    pd.Series(index=sites, data=cluster_labels, name="cluster").to_csv(
+        output_dir / "site_clusters.csv"
+    )
+
+    # Create figure
+    fig = go.Figure()
+
+    colors = [
+        "red",
+        "blue",
+        "green",
+        "orange",
+        "purple",
+        "black",
+        "yellow",
+        "pink",
+        "brown",
+        "grey",
+    ]
+    for ix, cur_cluster in enumerate(np.unique(cluster_labels)):
+        cur_mask = cluster_labels == cur_cluster
+        fig.add_trace(
+            go.Scattermapbox(
+                lat=station_df.loc[sites].loc[cur_mask].lat,
+                lon=station_df.loc[sites].loc[cur_mask].lon,
+                mode="markers",
+                marker=dict(size=10, color=colors[ix]),
+            )
+        )
+
+    # fig.update_layout(height=600, margin=dict(l=0, r=0, t=0, b=0))
+    fig.update_mapboxes(
+        accesstoken="pk.eyJ1IjoiY3MyMyIsImEiOiJjbGtpeXIxNnkwbDQ3M25xbDFrZWFnNHo3In0.OD7TJ_1PegpGvCOCxfHsnA",
+        center=dict(
+            lat=station_df.lat.mean(),
+            lon=station_df.lon.mean(),
+        ),
+        zoom=8,
+    )
+    fig.write_html(output_dir / "site_clusters.html")
+
+
+if __name__ == "__main__":
+    app()
